c97d562d19786e95a7345a853ab94f989b3710a8
Kyuhee Lee <kyuhee.lee.1011@gmail.com>
2023-09-12 03:28:46 -0400

    done

--- server/models.py
+++ server/models.py
@@ -12,6 +12,19 @@ class Author(db.Model):
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
+    @validates ('name')
+    def validate_name(self, key, name):
+        if name == "":
+            raise ValueError ("Name is not found")
+        return name
+    
+    @validates('phone_number')
+    def validate_phone_number(self, key, number):
+        if len(number) !=10:
+            raise ValueError ("Phone number must be 10 digits")
+        return number
+    
+    
     def __repr__(self):
         return f'Author(id={self.id}, name={self.name})'
 
@@ -26,6 +39,28 @@ class Post(db.Model):
     summary = db.Column(db.String)
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    @validates('title')
+    def validate_title(self, key, title):
+        if title not in ["Won't Believe","Secret","Top","Guess"]:
+            raise ValueError ("Title is not clickbaitable")
+        return title
+    @validates('content')
+    def validate_content(self, key, content):
+        if len(content) < 250:
+            raise ValueError("Contents must be at least 250 characters long")
+        return content
+    @validates('summary')
+    def validate_summary(self, key, summary):
+        if len(summary) > 250:
+            raise ValueError("Summary must be less than 250 characters")
+        return summary
+    @validates('category')
+    def validate_category(self, key, categories):
+        if categories not in ["Fiction", "Non-Fiction"]:
+            raise ValueError("Category must be Fiction or Non Fiction")
+        return categories
+    
 
 
     def __repr__(self):
